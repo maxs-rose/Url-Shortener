b2029961fd09a1d7fb26a6a410f2f3766a114662
Max Rose <max.rose366@gmail.com>
2022-05-31 14:53:45 +0100

    add trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
+import { AppRouter } from "@api/trpc/[trpc]";
 import '@styles/globals.css'
+import { withTRPC } from "@trpc/next";
 import type { AppProps } from 'next/app'
+import Head from "next/head";
+import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+      <>
+        <Head>
+          <title>Url Shortener</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+  )
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+  config({ctx}) {
+    const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/trpc` : `http://localhost:3000/api/trpc`;
+
+    return {
+      url
+    };
+  }
+})(MyApp);
---
+++ src/utils/trpc.ts
@@ -0,0 +1,4 @@
+import { AppRouter } from "@api/trpc/[trpc]";
+import { createReactQueryHooks } from "@trpc/react";
+
+export const trpc = createReactQueryHooks<AppRouter>()
