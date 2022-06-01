b759a942e442ab40f6a654d785121aeea4f4108b
Max Rose <max.rose366@gmail.com>
2022-06-01 13:07:24 +0100

    Fixed trpc url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,12 +17,16 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 	)
 }
 
+function getBaseUrl() {
+	if (typeof window !== undefined) return '';
+
+	return process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}`: `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export default withTRPC<AppRouter>({
 	config({ ctx }) {
-		const url = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/trpc` : `http://localhost:3000/api/trpc`;
-
 		return {
-			url
+			url: getBaseUrl()
 		};
 	},
 	ssr: true,
